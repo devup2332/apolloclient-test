7dabbae511334482b9493b9ae3e1474a7ed95469
Diego Rojas <devup2332@gmail.com>
2022-03-20 19:03:01 -0500

    Added queries for create and update

--- src/graphql/client/services/persons/queries.tsx
+++ src/graphql/client/services/persons/queries.tsx
@@ -1,10 +1,54 @@
 import { gql } from "@apollo/client";
 
-export const getPersons = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
+export const GET_USERS = gql`
+  query {
+    getUsers {
+      id
+      name
+      address
+      password
+      phone
+    }
+  }
+`;
+
+gql`
+  fragment user on User {
+    id
+    name
+  }
+`;
+
+export const GET_USER = gql`
+  query getUser($id: ID!, $whithPosts: Boolean!) {
+    getUser(id: $id) {
+      ...user
+      posts @include(if: $whithPosts) {
+        id
+        content
+      }
+    }
+  }
+`;
+
+export const CREATE_USER = gql`
+  mutation CREATE_USER(
+    $name: String
+    $password: String
+    $phone: String
+    $address: String
+  ) {
+    createUser(
+      name: $name
+      password: $password
+      phone: $phone
+      address: $address
+    ) {
+      name
+      password
+      phone
+      address
+      id
     }
   }
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,80 @@
-import { useQuery } from "@apollo/client";
-import React, { useEffect } from "react";
-import { getPersons } from "../graphql/client/services/persons/queries";
+import { useMutation, useQuery } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import {
+  CREATE_USER,
+  GET_USERS,
+} from "../graphql/client/services/persons/queries";
 
 const HomePage = () => {
-  const { data, error, loading } = useQuery(getPersons);
-  const handleGetPersons = () => {
-    console.log("Ger persons", data);
+  const [person, setPerson] = useState<any>({
+    name: "",
+    password: "",
+    phone: "",
+    address: "",
+  });
+  const { data: users, loading, error } = useQuery(GET_USERS);
+  const [createPersonMethod, { data: newUser }] = useMutation(CREATE_USER, {
+    refetchQueries: [
+      {
+        query: GET_USERS,
+      },
+    ],
+  });
+
+  const createPerson = async (e: any) => {
+    e.preventDefault();
+    await createPersonMethod({
+      variables: {
+        name: person.name,
+        password: person.password,
+        phone: person.phone,
+        address: person.address,
+      },
+    });
+  };
+  const onChange = (e: any) => {
+    setPerson({
+      ...person,
+      [e.target.name]: e.target.value,
+    });
   };
   useEffect(() => {
-    console.log(data);
-  }, [data]);
+    console.log("users", users);
+  }, [users]);
+
+  if (error) return <p>{error}</p>;
   return (
     <div>
-      {loading && <p>Cargando</p>}
-      {data?.rates &&
-        data?.rates?.map((i: any, index: number) => {
-          return <p key={index}>{i.currency}</p>;
-        })}
+      <form onSubmit={createPerson}>
+        <input
+          placeholder="Name"
+          name="name"
+          value={person.name}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          placeholder="Password"
+          name="password"
+          value={person.password}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          placeholder="Phone"
+          name="phone"
+          value={person.phone}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          placeholder="Address"
+          name="address"
+          value={person.address}
+          onChange={(e) => onChange(e)}
+        />
+        <button type="submit">Create Person</button>
+      </form>
+      {users?.getUsers.map((u: any, index: number) => {
+        return <p key={index}>{u.name}</p>;
+      })}
     </div>
   );
 };
